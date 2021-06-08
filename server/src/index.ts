35068bf73b3576ad5776e029107821c48c349c62
zephyrus <pandey.piyus@gmail.com>
2021-06-08 21:39:45 +0530

    adding mikroORM integration with PSQL 🎣

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config';
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: 'hello bitch' });
+  // await orm.em.persistAndFlush(post);
+
+  const post = await orm.em.find(Post, {});
+  console.log(post);
+};
+
+main();
