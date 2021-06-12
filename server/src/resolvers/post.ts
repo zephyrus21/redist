6246a03ed16e6279f607887acc99d03348dbb2f9
zephyrus <pandey.piyus@gmail.com>
2021-06-12 18:55:15 +0530

    adding post resolvers 🔔

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,11 +1,54 @@
 import { Post } from '../entities/Post';
 import { MyContext } from '../types';
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     return ctx.em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg('id') id: number, @Ctx() ctx: MyContext): Promise<Post | null> {
+    return ctx.em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post> {
+    const post = ctx.em.create(Post, { title });
+    await ctx.em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title') title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post | null> {
+    const post = await ctx.em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    post.title = title;
+    await ctx.em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<Boolean> {
+    try {
+      ctx.em.nativeDelete(Post, { id });
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
