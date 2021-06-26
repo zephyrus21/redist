ddf5eba3f3758317004dce0180b92042cc2b8246
zephyrus <pandey.piyus@gmail.com>
2021-06-26 18:00:00 +0530

    adding login and register funcs 🎺

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -4,9 +4,10 @@ import {
   Field,
   InputType,
   Mutation,
-  Query,
+  ObjectType,
   Resolver,
 } from 'type-graphql';
+import argon2 from 'argon2';
 import { User } from '../entities/User';
 import { MyContext } from '../types';
 
@@ -18,14 +19,84 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => String)
+  @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
     @Ctx() ctx: MyContext
-  ) {
-    const user = ctx.em.create(User, { username: options.username });
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'username must have more than 2 characters',
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = ctx.em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
     await ctx.em.persistAndFlush(user);
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() ctx: MyContext
+  ): Promise<UserResponse> {
+    const user = await ctx.em.findOne(User, {
+      username: options.username,
+    });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: "username doesn't exist",
+          },
+        ],
+      };
+    }
+
+    const validPassword = await argon2.verify(user.password, options.password);
+    if (!validPassword) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'incorrect password',
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
